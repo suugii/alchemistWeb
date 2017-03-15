5cee7edd42c71ad546c7764afb1b2644c27cb4c1
byambaa122 <byambaa.122@gmail.com>
2017-03-15 18:04:38 +0800

    translated Calendar

--- resources/views/en/crm.blade.php
+++ resources/views/en/crm.blade.php
@@ -322,7 +322,16 @@
 	        }
 		};
 		$('#calendar').calendar({
-			type: 'date'
+			type: 'date',
+			text: {
+				days: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+				months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+				monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+				today: 'Today',
+				now: 'Now',
+				am: 'AM',
+				pm: 'PM'
+			},
 		});
 		$('#price-offer').submit(function(e) {
 	    	e.preventDefault(); 
